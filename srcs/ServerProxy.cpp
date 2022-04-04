a994dec9df715933ef4d79dd89da4120bd25c1f0
3danger <3danger@mail.ru>
2022-04-04 19:13:18 +0300

    added comments and smallfix, removed unused variable

--- srcs/ServerProxy.cpp
+++ srcs/ServerProxy.cpp
@@ -3,28 +3,55 @@
 //
 
 #include "ServerProxy.h"
-
+/*
+ * port и ipAddres для нашего прокси сервера
+ */
 ServerProxy::ServerProxy(char const * port, char const * ipAddres)  {
+	// Зануляем то что пока не используется.
 	clientFD = -1;
 	serverFD = -1;
+	/*
+	 * Записываем информацию,
+	 * номер порта,
+	 * IP адрес,
+	 * и тип подключения
+	 * в структуру proxyInfo
+	 */
 	addrinfo hints = makeAddrinfoHints();
 	if (getaddrinfo(ipAddres, port, &hints, &proxyInfo))
 		throw std::runtime_error(strerror(errno));
+	// Получаем сокет/FD подробнее см. в комментариях самой функции makeSocket();
 	maxFd = proxySocketFD = makeSocket();
+	// Связываем наш пустой сокет с конкретным адресом который -
 	makeBind(proxySocketFD, proxyInfo);
-	int resListen = listen(proxySocketFD, 1);
+	// - Будем слушать в listen
+	int resListen = listen(proxySocketFD, 5);
 	if (resListen < 0)
 		throw std::runtime_error("Listen error");
+
+	/*
+	 * Для начала искользования fd_set fds
+	 * необходимо его занулить с помощью FD_ZERO
+	 * и лишь затем добавить в него FD
+	 * с помошью FD_SET()
+	 * который далее будет использоваться в методе select
+	 */
 	FD_ZERO(&fds);
 	FD_SET(proxySocketFD, &fds);
-	loop = true;
 }
 
 ServerProxy::~ServerProxy() {
+	/*
+	 * Зачищаем память и
+	 * закрываем открытые сокеты,
+	 * иначе будет утечка.
+	 */
 	close(proxySocketFD);
 	std::cout << "proxySocketFD was closed from destructor!" << std::endl;
 	freeaddrinfo(proxyInfo);
 	std::cout << "proxyInfo was freed from destructor!" << std::endl;
+	freeaddrinfo(serverInfo);
+	std::cout << "serverInfo was freed from destructor!" << std::endl;
 	close(clientFD);
 	std::cout << "clientFD was closed from destructor!" << std::endl;
 	close(serverFD);
@@ -32,16 +59,25 @@ ServerProxy::~ServerProxy() {
 }
 
 int ServerProxy::makeSocket() {
-
+	/*
+	 * Создаем сокет/FD
+	 * который будет работать с ipv4
+	 * и иметь тип подключения TCP
+	 */
 	int res = socket(AF_INET, SOCK_STREAM, 0);
 	if (res < 0)
 		throw std::runtime_error("Socket error");
-	fcntl(res, F_SETFD, fcntl(res, F_GETFD) | O_NONBLOCK);
+	// Задаем флаг для нашего сокета как неблокирующий
+	fcntl(res, F_SETFD, O_NONBLOCK);
 	std::cout << "socket created" << std::endl;
 	return res;
 }
 
 void ServerProxy::makeBind(int fd, addrinfo * addr)  {
+	/*
+	 * Одноразовая функция которая связывает
+	 * прослушиываемый порт с нашим FD
+	 */
 	int res = bind(fd, addr->ai_addr, addr->ai_addrlen);
 	if (res < 0)
 		throw std::runtime_error(std::string("Bind error: ") + strerror(errno));
@@ -49,25 +85,35 @@ void ServerProxy::makeBind(int fd, addrinfo * addr)  {
 }
 
 int ServerProxy::connectToClient() {
+	 // Принимаем подключение
 	clientFD = accept(proxySocketFD, proxyInfo->ai_addr, &proxyInfo->ai_addrlen);
-	if (clientFD < 0 && errno != EAGAIN)
+	if (clientFD < 0)
 		std::runtime_error(std::string("connection to client err: ") + strerror(errno));
+	// Добавляем его в набор дескрипторов
 	FD_SET(clientFD, &fds);
+	// maxFd нужен для метода select
 	maxFd = std::max(maxFd, clientFD);
 	return	clientFD;
 }
 
 int ServerProxy::connectToServer(const char *port, const char *ipAddres)  {
+	// Задаем параметры сервера к которобу будем подключаться
 	addrinfo hints = makeAddrinfoHints();
 	if (getaddrinfo(ipAddres, port, &hints, &serverInfo))
 		throw std::runtime_error(strerror(errno));
+	// Создаем пустой сокет
 	serverFD = makeSocket();
-	if (connect(serverFD, (sockaddr *)serverInfo->ai_addr, serverInfo->ai_addrlen))
-		throw std::runtime_error(std::string("cannot connect to server: ") + strerror(errno));
-	if (serverFD < 0 && errno != EAGAIN)
+	if (serverFD < 0)
 		throw std::runtime_error(strerror(errno));
+	// Подключаемся к серверу и ассоциируем этот совет к подключаемому серверу
+	if (connect(serverFD, (sockaddr *)serverInfo->ai_addr, serverInfo->ai_addrlen)) {
+		throw std::runtime_error(std::string("cannot connect to server: ") + strerror(errno));
+	}
+	// Задаем флаг сокету как неблокирующий
 	fcntl(serverFD, F_SETFD, O_NONBLOCK);
+	// Добавляем в набор сокетов в fds, это нужно для метода select
 	FD_SET(serverFD, &fds);
+	// maxFd нужен для метода select
 	maxFd = std::max(maxFd, serverFD);
 	return serverFD;
 }
@@ -76,30 +122,53 @@ int ServerProxy::connectToServer(const char *port, const char *ipAddres)  {
 	int numOfSelect;
 	uint8_t whatLast = 0;
 	fd_set copyFds;
-	timeval tm = {0, 50000};
+	timeval tm = {0, 50000}; // Тайм-оут для select
 
+	// Зачищаем буффер перед использованием, хотя это здесь не нужно
 	bzero(buff, SIZE_BUFF);
-	fcntl(clientFD, F_SETFD, O_NONBLOCK);
-	fcntl(serverFD, F_SETFD, O_NONBLOCK);
 	std::ofstream outFileLog;
 
 	while(true){
+		/*
+		 * Делаем копию fds,
+		 * потому что select удаляет из набора те сокеты,
+		 * которых не появились данные.
+		 */
 		copyFds = fds;
+		// тут мы удаляем из набора copFds те сокеты в которых ничего не произошло, те не появились данные
+		// select так же возвращает число означающее количество сокетов в которых есть что прочитать
 		numOfSelect = select(maxFd + 1, &copyFds, NULL, NULL, &tm);
 		if (numOfSelect < 0)
 			throw std::runtime_error(std::string("select error: ") + strerror(errno));
 		if (numOfSelect > 0){
+			/*
+			 * открываем наш лог файл для записи,
+			 * так же переносим 'курсор' в конец файлы, чтоб не перезаписывала поверх.
+			 * если файла нет, то он будет создан
+			 */
 			outFileLog.open(logFileName, std::ios::app);
+			/*
+			 * FD_ISSET проверяет есть ли этот сокет/FD в наборе copyFds -
+			 * который имеет только те FDs - в которых есть данные
+			 */
 			if (FD_ISSET(serverFD, &copyFds)) {
-				if (whatLast != 1)
+				/*
+				 * whatLast != FD
+				 * это условие необходимо для того что бы 2
+				 * раза подряд не писать в лог файле откуда пришло сообщение
+				 */
+				if (whatLast != serverFD)
 					outFileLog << "\n\nSERVER REPLY:\n";
-				whatLast = 1;
+				whatLast = serverFD;
+				// Отправляем сообщение от сервера к клиенту если там был мессендж
 				Send(serverFD, clientFD, &copyFds, outFileLog);
 			}
 			if (FD_ISSET(clientFD, &copyFds)) {
-				if (whatLast != 2)
+				if (whatLast != clientFD) {
 					outFileLog << "\n\nCLIENT REPLY:\n";
-				whatLast = 2;
+				}
+				whatLast = clientFD;
+				// Отправляем сообщение от клиента к серверу если там был мессендж
 				Send(clientFD, serverFD, &copyFds, outFileLog);
 			}
 			outFileLog.close();
@@ -107,20 +176,43 @@ int ServerProxy::connectToServer(const char *port, const char *ipAddres)  {
 	}
 }
 
+/*
+ * Читаем сообщения из \from и
+ * отправляем сообщение в \to
+ */
 void ServerProxy::Send(int from, int to, fd_set * fds_set, std::ofstream & outFileLog){
+	/*
+	 * Удаляем данный FD из набора непрочитанных сокетов,
+	 * тут это пока не нужно,
+	 * но в случае работы со множеством сокетов
+	 * это будет необходимо.
+	 */
 	FD_CLR(from, fds_set);
+	// Читаем сообщение с \from в наш буффер. так же получаем колличество прочитаннх байт
 	int readedBytes = recv(from, buff, SIZE_BUFF, 0);
+	// Отправляем прочитанные сообщение к \to
 	send(to, buff, readedBytes, 0);
-	for(int i = 0; i < readedBytes; ++i)
-		if (buff[i] == '\u0002')
-			readedBytes = i;
+	// Удаляем служебый символ который не нужен для лог файла
+	// это наверное костыль, при необходимости придумаю чтонибудь
+	for(int i = 0; i < readedBytes; ++i) {
+		if (buff[i] == '\u0002') {
+			buff[i] = ' ';
+		}
+	}
+	// Логгируем месендж
 	outFileLog.write(buff, readedBytes);
+	// Зачищаем буфер
 	bzero(buff, readedBytes);
 }
 
+// Создаем структуру с базовой информацией для использования по дефолту
 addrinfo ServerProxy::makeAddrinfoHints() {
 	addrinfo hints = {0};
 
+	/*
+	* AF_INET говорит нам что нужно использовать тип ipv4
+	* SOCK_STREAM этим указываем TCP
+	*/
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	return hints;
--- srcs/ServerProxy.h
+++ srcs/ServerProxy.h
@@ -24,28 +24,33 @@
 #define SIZE_BUFF 128
 
 class ServerProxy {
-	bool loop;
-
-	int proxySocketFD;
-	int clientFD;
-	int serverFD;
-	addrinfo * proxyInfo;
-	addrinfo * serverInfo;
-	char buff[SIZE_BUFF];
-	int maxFd;
-	fd_set fds;
-	std::string logFileName = "log.txt";
+	int proxySocketFD;	// Сокет этого прокси-сервера
+	int clientFD;		// Сокет клиента
+	int serverFD;		// Сокет сервера
+	addrinfo * proxyInfo;	// Информация о нашем прокси
+	addrinfo * serverInfo;	// Информация о удаленном сервере
+	char buff[SIZE_BUFF];	// Буффер для обмена сообщений
+	int maxFd;		// Маскимальное id of FD, необходим для select
+	fd_set fds;		// Тут будем хранить все наборы FD, в нашем случае их будет всего 3
+	std::string logFileName = "log.txt"; // Имя лог файла;
 public:
 	ServerProxy(char const * port, char const * ipAddres);
 	~ServerProxy();
 
 	[[noreturn]] void run();
+	// Принимаем подключение клиента
 	int connectToClient();
+	// Подключаемся к серверу по его адресу
 	int connectToServer(char const * port, char const * ipAddres);
 private:
+	// Создаем сокет типа TCP ipv4
 	static int makeSocket();
+	// Связвываем сокет с конкрентым адресом
 	static void makeBind(int fd, addrinfo * addr);
+	// Создаем структуру с базовой информацией для использования по дефолту
 	addrinfo makeAddrinfoHints();
+	// Читаем сообщение из FD \from, и отправляем это сообщение по FD \to,
+	// при этом логгируя это в файле outFileLog
 	void Send(int from, int to, fd_set * fds_set, std::ofstream & outFileLog);
 };
 
